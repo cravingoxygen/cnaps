0ec662774397be9065904ba69c2eb2fc7d831ed7
Elre <vze.ezv@gmail.com>
2020-05-04 15:45:50 +0100

    Alternative way of specifying the constant part of the context set in the model_wrapper function

--- src/run_cnaps.py
+++ src/run_cnaps.py
@@ -313,22 +313,28 @@ class Learner:
 
                 task_dict = self.dataset.get_test_task(item, session)
                 context_images, target_images, context_labels, target_labels, context_images_np = self.prepare_task(task_dict, shuffle=False)
-                context_images_attack_all = context_images.detach()
+                import pdb; pdb.set_trace()
+                # Detach shares storage with the original tensor, which isn't what we want.
+                context_images_attack_all = context_images.clone()
+                # Is require_grad true here, for context_images?
 
                 for c in range(0,1): #torch.unique(context_labels):
-                    #Adversarial input context image
-                    class_index = self.model._extract_class_indices(context_labels, c)[0]
+                    # Adversarial input context image
+                    class_index = self.model._extract_class_indices(context_labels, c)[0].item()
                     context_x = np.expand_dims(context_images_np[class_index], 0)
 
-                    context_images_attack = context_images.detach()
-
-                    #Iput to the model wrapper is automatically converted to Torch tensor for us
+                    # Iput to the model wrapper is automatically converted to Torch tensor for us
                     def model_wrapper(context_point_x):
-                        context_images_attack[class_index] = context_point_x
+                        # Insert context_point at correct spot
+                        context_images_attack = torch.cat([context_images[0:class_index], context_point_x, context_images[class_index+1:-1]], dim=0)
+
+                        # Alternatively, we have to recreate context_images in each call to the wrapper function
+                        # context_images_attack = context_images.detach()
+                        # context_images_attack[class_index] = context_point_x
+
                         target_logits = self.model(context_images_attack, context_labels, target_images)
                         return target_logits[0]
 
-                    import pdb; pdb.set_trace()
                     tf_model_conv = convert_pytorch_model_to_tf(model_wrapper, out_dims=self.args.way)
                     tf_model = cleverhans.model.CallableModelWrapper(tf_model_conv, 'logits')
                     pgd = ProjectedGradientDescent(tf_model, sess=session, dtypestr='float32')
@@ -338,6 +344,7 @@ class Learner:
                     preds_adv_op = tf_model.get_logits(adv_x_op)
 
                     feed_dict = {x: context_x}
+                    import pdb; pdb.set_trace()
                     adv_x, preds_adv = session.run((adv_x_op, preds_adv_op), feed_dict=feed_dict)
                     import pdb; pdb.set_trace()
                     context_images_attack_all[class_index] = torch.from_numpy(adv_x)
